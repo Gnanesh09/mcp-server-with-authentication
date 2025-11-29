542969fda087c2464a2e65fa07dcfe286dc297d0
Gnanesh G <gnanesh9206@gmail.com>
2025-11-29 20:47:31 +0530

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from mcp-auth-project!")
+from fastmcp import FastMCP
+from dotenv import load_dotenv
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+load_dotenv()
+
+mcp  =FastMCP(name="Notes app")
+
+@mcp.tool()
+def get_my_notes():
+    """get all notes for a user"""
+    return "no no notes"
+
+@mcp.tool()
+def add_note(content:str)->str:
+    return F"added note: {content}"
+
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(
+        transport="http",
+        host = "127.0.0.1",
+        port = 8000,
+        middleware = [
+            Middleware(CORSMiddleware,
+                       allow_origins=["*"],
+                       allow_credentials = True,
+                       allow_methods = ["*"],
+                       allow_headers =["*"] 
+                       
+                       )
+            
+        ]
+    )
